eff6624489265e88cb2a02247498d6e2a5879d85
Daniel Moore <hi@danielmoore.us>
2018-12-14 04:10:26 -0800

    Solved 7.3.

--- ListNode.java
+++ ListNode.java
@@ -1,6 +1,6 @@
 public class ListNode<T> {
-	public T data;
-	public ListNode<T> next;
+    public T data;
+    public ListNode<T> next;
 
     public ListNode(T data) {
         this.data = data;
---
+++ _73TestListForCycle.java
@@ -0,0 +1,22 @@
+public class _73TestListForCycle {
+    public static boolean testForCycle(ListNode<Integer> list) {
+        ListNode<Integer> head = list;
+        list = list.next;
+        for (int i = 1; list != null; i++, list=list.next) {
+            ListNode<Integer> headCopy = head;
+            for (int j = 0; j < i; j++) {
+                if (headCopy.equals(list)) {
+                    return true;
+                }
+                headCopy = headCopy.next;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode<Integer> list = new ListNode<>(new Integer[]{1,2});
+        list.next.next = list;
+        System.out.println(testForCycle(list));
+    }
+}
